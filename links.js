e0912c340657c26d53ba2557aec46e8836a96d5c
Kyle <[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C Piacenta[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DPiacentakyl@gmail.com>
2023-11-15 15:01:30 -0500

    just checking

---
+++ links.js
@@ -0,0 +1,10 @@
+
+
+function changeHeaderToMessage(){
+    event.preventDefault();
+    var headerTextChange = document.getElementById('header').innerText = 'Send Me a Message!';
+}
+
+function headerDoubleClick(){
+    var headerText = document.getElementById('header').innerText = 'My Personal Links!'
+}
