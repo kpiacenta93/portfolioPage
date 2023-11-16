7a48996f7532b338b4ea1dee065050a24701fa49
Kyle <[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C Piacenta[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DPiacentakyl@gmail.com>
2023-11-15 21:31:41 -0500

    styled links/messageForm section

--- links.js
+++ links.js
@@ -1,10 +1,29 @@
 
 
-function changeHeaderToMessage(){
+/*function changeHeaderToMessage(){
     event.preventDefault();
     var headerTextChange = document.getElementById('header').innerText = 'Send Me a Message!';
+
+    alert('your message has been sent')
 }
 
 function headerDoubleClick(){
     var headerText = document.getElementById('header').innerText = 'My Personal Links!'
+} */
+
+
+
+function changeHeaderToMessage(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('messageForm');
+    
+    if (form.classList.contains('show')) {
+        form.classList.remove('show');
+        setTimeout(() => form.style.display = 'none', 1000); 
+    } else {
+        form.style.display = 'block';
+        setTimeout(() => form.classList.add('show'), 50); 
+    }
 }
+
