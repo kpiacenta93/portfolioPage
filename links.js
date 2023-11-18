fb63338d50e9fce8b02ca6ca6a1765417e1ccf58
Kyle <[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C Piacenta[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DPiacentakyl@gmail.com>
2023-11-18 17:34:20 -0500

    edited form/links page and added background image for both home and links page

--- links.js
+++ links.js
@@ -1,15 +1,8 @@
 
 
-/*function changeHeaderToMessage(){
-    event.preventDefault();
-    var headerTextChange = document.getElementById('header').innerText = 'Send Me a Message!';
 
-    alert('your message has been sent')
-}
 
-function headerDoubleClick(){
-    var headerText = document.getElementById('header').innerText = 'My Personal Links!'
-} */
+
 
 
 
@@ -17,13 +10,35 @@ function changeHeaderToMessage(event) {
     event.preventDefault();
 
     const form = document.getElementById('messageForm');
-    
+    const container = document.getElementById('container');
+    const header = document.getElementById('header'); 
+
     if (form.classList.contains('show')) {
         form.classList.remove('show');
-        setTimeout(() => form.style.display = 'none', 1000); 
+        setTimeout(() => form.style.display = 'none', 300); 
+        container.style.display = ''; 
+        header.innerText = 'My Personal Links!'; 
     } else {
         form.style.display = 'block';
-        setTimeout(() => form.classList.add('show'), 50); 
+        setTimeout(() => form.classList.add('show'), 50);
+        container.style.display = 'none'; 
+        header.innerText = 'Send Me A Message!'; 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const openMessageForm = document.getElementById('sendMessage');
+    openMessageForm.addEventListener('click', changeHeaderToMessage);
+
+    const backToLinks = document.getElementById('backToLinks');
+    backToLinks.addEventListener('click', changeHeaderToMessage)
+});
+
+
+
+
+
+
+
+
 
